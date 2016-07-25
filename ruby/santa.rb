cee2cab05904ce683a73b56284968d9ab467d252
Kyle <KylePDorsey@gmail.com>
2016-07-25 11:01:07 -0500

    started santa class and 3 methods

---
+++ ruby/santa.rb
@@ -0,0 +1,16 @@
+
+
+
+class Santa
+	def initialize
+		"Initializing Santa instance ..."
+	end
+
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}" 
+	end
+end
