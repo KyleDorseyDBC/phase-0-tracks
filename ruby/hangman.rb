d76dea51b649d4d81ae2e5ba79433c7740adbb1c
Kyle Dorsey <KylePDorsey@gmail.com>
2016-07-28 15:47:23 -0500

    nearly done

--- ruby/hangman.rb
+++ ruby/hangman.rb
@@ -11,35 +11,55 @@ class Hangman
 
   def number_of_guesses
     total_guesses = @correct_answer.length
-    remaining_guesses = total_guesses - @guess_count #later add a method to subtract as each letter is guessed
-    remaining_guesses
+    @remaining_guesses = total_guesses - @guess_count 
+    @remaining_guesses
+  end
+
+  def used_a_guess
+    @remaining_guesses += -1
+    @remaining_guesses
   end
 
   def create_empty_array
     empty_array_length = @correct_answer.length.to_i
     empty_array_length.times {|blank| @guessed_array << "_"}
-    @guessed_array.join(" ") #returns out "_ _ _ _ _ _ _  _"
-    p @guessed_array
+    @guessed_array.join(" ")
   end
 
   def solved_portion(letter)
     letter_index = @correct_answer.index(letter)
     @guessed_array.insert(letter_index, letter)
+    p letter
+    p @guessed_array
     @guessed_array.delete_at(letter_index + 1)
     p @guessed_array
   end
 
   def check_guess(letter)
     if @guessed_array.include?(letter) == true
-      #you have already guessed this letter, dosent take a guess
+      puts "you have already guessed this letter, this wont take a guess"
+      printer
     elsif @correct_answer.include?(letter) == true
       solved_portion(letter)
-      #set it into the array with blanks
-      @guess_count += 1
-
+      used_a_guess
+      printer
     else
-      @guess_count += 1
-      #incorrect guess
+      used_a_guess
+      printer
+    end
+  end
+
+  def printer
+    if @guess_count >= @remaining_guesses
+      if @guessed_array.include?('_')
+        puts "Sorry, better luck next time"
+        return
+      else
+        puts "Congrats you won! The word was #{@correct_answer.join(' ').delete(' ').capitalize}"
+        return
+      end
+    else
+      puts "Your current progress is : #{@guessed_array.join(' ')}, You have #{@remaining_guesses} guesses left!"
     end
   end
 end
@@ -47,4 +67,12 @@ end
 hangman = Hangman.new("sandwich")
 hangman.number_of_guesses
 hangman.create_empty_array
+hangman.check_guess("s")
 hangman.check_guess("a")
+hangman.check_guess("n")
+hangman.check_guess("w")
+hangman.check_guess("h")
+hangman.check_guess("i")
+hangman.check_guess("c")
+hangman.check_guess("d")
+
