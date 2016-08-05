3ad66ebf34f54be785f28bd84e3361457ce25c79
Kyle Dorsey <KylePDorsey@gmail.com>
2016-08-05 10:41:38 -0500

    Initial commit

---
+++ js/algos.js
@@ -0,0 +1,17 @@
+//function that takes an array of words
+//  returns the longest word or phrase in the array
+//  iterate through the array to find the greatest .length 
+//  define variables to hold the longest string and length of the longest string
+// loop through the array
+  // if item length > longest length  redefine longest string and longest length variables
+// print longest string var when done
+
+function LongestWord(words){
+  var longest_string = ""
+  var lengthofString = 0
+  for (words.length > lengthofString) {
+    var longest_string = words
+    var lengthofString = words.length
+  }
+console.log("Here is the output")
+}
