32ebd9d5b673e828fc4b51acd8e25919ec49a805
Kyle <KylePDorsey@gmail.com>
2016-08-17 10:28:41 -0500

    Completed 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,6 +1,7 @@
 # require gems
 require 'sinatra'
 require 'sqlite3'
+require 'faker'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
@@ -44,3 +45,33 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+#Release 0
+
+get '/contact' do
+	Faker::Address.street_address
+end
+
+get '/great_job' do
+	"Good job, #{params[:name]}!"
+end
+
+##http://127.0.0.1:9393/8/plus/10
+get '/:first_num/plus/:second_num' do
+	answer = params[:first_num].to_i + params[:second_num].to_i
+	answer.to_s
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
